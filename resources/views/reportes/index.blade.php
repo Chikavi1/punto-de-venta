ed1fbcf37506dab97c2944662e54ff2e5e1c154a
chikavi1 <chikavi@hotmail.com>
2019-02-11 10:31:39 -0600

    agregue la vista de los reportes

---
+++ resources/views/reportes/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<script>
+	 $(document).ready(function(){
+    $('.datepicker').datepicker();
+    console.log($('#inicios').val());
+  });
+</script>
+	<div class="row">
+		<div class="col m10 offset-m1 card">
+			<h4 class="center-align">Generar Reporte</h4>
+			<p class="center-align">Selecciona la fecha con la cual quieres generar el reporte.</p>
+			<form  method="GET" class="col s12">
+				<div class="row">
+					<div class="input-field col s5 m4 ">
+						<input type="search" class="datepicker"  placeholder="fecha de inicio" id="inicios" name="FInicio">
+					</div>
+					<div class="input-field col s2 m4">
+					</div>
+					<div class="input-field col s5 m4 ">
+						<input type="text"  class="datepicker" placeholder="fecha de finalizacion" name="FFinal">
+					</div>
+					<input type="submit" value="Generar" class="btn btn-block color-cut">
+				</div>
+			</form>
+		</div>
+	</div>
+
+	@foreach($resultado as $res)
+	{{ $res->id }}
+	@endforeach
+@endsection
