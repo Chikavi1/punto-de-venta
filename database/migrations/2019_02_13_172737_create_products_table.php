2408a6d79db022e79c12229803ce0c7f6faec696
chikavi1 <chikavi@hotmail.com>
2019-02-14 20:29:13 -0600

    agregue la tabla de productos a la bases de datos

---
+++ database/migrations/2019_02_13_172737_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigoBarra');
+            $table->string('imagen');
+            $table->integer('cantidad');
+            $table->string('categoria');
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->decimal('costo');
+            $table->decimal('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
